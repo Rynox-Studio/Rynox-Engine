3defc3f5612b59b5c66e93efa5ebd058a1250d8c
MoRGeoS <kidalovo12345678910@gmail.com>
2026-02-25 08:31:26 -0700

    Create main.cpp

---
+++ RynoxEditor/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Core/Application.h>
+using namespace Rynox::Core;
+
+#include <Common/Logger.h>
+
+int main()
+{
+	Application* app = nullptr;
+	
+	try
+	{
+		app = new Application();
+
+		if (!app->Initialize({}))
+		{
+			return EXIT_FAILURE;
+		}
+
+		app->Run();
+	}
+	catch (const std::exception& e)
+	{
+		RNX_LOG_ERROR("Exception: {}", e.what());
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
