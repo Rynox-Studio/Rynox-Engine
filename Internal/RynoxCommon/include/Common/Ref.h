f16d4b66adb8f415e65d796a2674c6967c90698a
MoRGeoS <kidalovo12345678910@gmail.com>
2026-04-14 10:57:30 -0600

    Dependencies moved

--- Internal/RynoxCommon/include/Common/Ref.h
+++ Internal/RynoxCommon/include/Common/Ref.h
@@ -9,10 +9,12 @@ namespace Rynox::Common
 	{
 	public:
 		RefCounted() = default;
-		virtual ~RefCounted() = default;
+		virtual ~RefCounted() = default;  
 
 		void IncRef() const { ++m_RefCount; }
 		void DecRef() const { --m_RefCount; }
+
+		virtual void Release() { delete this; }
 
 		[[nodiscard]] uint32_t GetRefCount() const { return m_RefCount.load(); }
 
@@ -20,41 +22,17 @@ namespace Rynox::Common
 		mutable std::atomic<uint32_t> m_RefCount{ 0 };
 	};
 
-    template<typename T>
+    template <typename T>
     concept RefCountedConcept = std::derived_from<T, RefCounted>;
 
-    template<typename T>
+    template <typename T>
 	requires RefCountedConcept<T>
 	class Ref
 	{
 	public:
 		Ref() = default;
-
-		Ref(std::nullptr_t n) : m_Instance(nullptr) {}
-
+		Ref(std::nullptr_t) : m_Instance(nullptr) {}
 		Ref(T* instance) : m_Instance(instance) { IncRef(); }
-
-		template<typename T2>
-		requires(std::convertible_to<T*, T2*> || std::convertible_to<T2*, T*>)
-		Ref(const Ref<T2>& other)
-		{
-			m_Instance = static_cast<T*>(other.m_Instance);
-			IncRef();
-		}
-
-		template<typename T2>
-		requires(std::convertible_to<T*, T2*> || std::convertible_to<T2*, T*>)
-		Ref(Ref<T2>&& other) noexcept
-		{
-			m_Instance = static_cast<T*>(other.m_Instance);
-			other.m_Instance = nullptr;
-		}
-
-		template<typename... Args>
-		[[nodiscard]] static Ref<T> Create(Args&&... args)
-		{
-			return Ref<T>(new T(std::forward<Args>(args)...));
-		}
 
 		~Ref() { DecRef(); }
 
@@ -83,8 +61,24 @@ namespace Rynox::Common
 			return *this;
 		}
 
-		template<typename T2>
-		requires(std::convertible_to<T*, T2*>)
+		template <typename T2>
+		requires std::convertible_to<T*, T2*> || std::convertible_to<T2*, T*>
+		Ref(const Ref<T2>& other)
+		{
+			m_Instance = static_cast<T*>(other.m_Instance);
+			IncRef();
+		}
+
+		template <typename T2>
+		requires std::convertible_to<T*, T2*> || std::convertible_to<T2*, T*>
+		Ref(Ref<T2>&& other) noexcept
+		{
+			m_Instance = static_cast<T*>(other.m_Instance);
+			other.m_Instance = nullptr;
+		}
+
+		template <typename T2>
+		requires std::convertible_to<T*, T2*> || std::convertible_to<T2*, T*>
 		[[nodiscard]] Ref<T2> As() const
 		{
 			return Ref<T2>(dynamic_cast<T2*>(m_Instance));
@@ -117,6 +111,13 @@ namespace Rynox::Common
 			m_Instance = nullptr;
 		}
 
+		template <typename... Args>
+		requires std::constructible_from<T, Args...>
+		[[nodiscard]] static Ref<T> Create(Args&&... args)
+		{
+			return Ref<T>(new T(std::forward<Args>(args)...));
+		}
+
 	private:
 		void IncRef() const
 		{
@@ -131,24 +132,32 @@ namespace Rynox::Common
 			if (m_Instance)
 			{
 				m_Instance->DecRef();
-
 				if (m_Instance->GetRefCount() == 0)
 				{
-					delete m_Instance;
+					T* temp = m_Instance;
+					m_Instance = nullptr;
+					temp->Release();
 				}
-				m_Instance = nullptr;
+				else
+				{
+					m_Instance = nullptr;
+				}
 			}
 		}
 
 	private:
-		template<typename T2>
+		template <typename T2>
 		requires RefCountedConcept<T2>
 		friend class Ref;
+
+		template <typename T2>
+		requires RefCountedConcept<T2>
+		friend class WeakRef;
 
 		mutable T* m_Instance = nullptr;
 	};
 
-	template<typename T>
+	template <typename T>
 	requires RefCountedConcept<T>
 	class WeakRef
 	{
@@ -157,6 +166,10 @@ namespace Rynox::Common
 		WeakRef(std::nullptr_t) : m_Instance(nullptr) {}
 		WeakRef(const Ref<T>& ref) : m_Instance(ref.Get()) {}
 
+		template <typename T2>
+		requires std::convertible_to<T2*, T*>
+		WeakRef(const Ref<T2>& ref) : m_Instance(dynamic_cast<T*>(ref.Get())) {}
+
 		[[nodiscard]] bool IsValid() const
 		{
 			return m_Instance != nullptr && m_Instance->GetRefCount() > 0;
@@ -164,11 +177,18 @@ namespace Rynox::Common
 
 		[[nodiscard]] Ref<T> Lock()
 		{
-			if (IsValid())
+			if (!m_Instance) return nullptr;
+
+			m_Instance->IncRef();
+			if (m_Instance->GetRefCount() == 1)
 			{
-				return Ref<T>(m_Instance);
+				m_Instance->DecRef();
+				return nullptr;
 			}
-			return nullptr;
+
+			Ref<T> result;
+			result.m_Instance = m_Instance;
+			return result;
 		}
 
 		void Reset() { m_Instance = nullptr; }
